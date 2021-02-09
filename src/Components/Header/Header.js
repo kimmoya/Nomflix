d9446b8e0f9a2cd063d063a177735b73a456acb8
kimmoya <kokokoro83@gmail.com>
2021-02-09 16:42:12 +0900

    3

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Header.module.css"
+export default () => (
+    <header>
+        <ul className = {styles.navList}>
+            <li>
+                <a href = "/"> Movies </a>
+            </li>
+            <li>
+                <a href = "/TV"> TV </a>
+            </li>
+            <li>
+                <a href = "/Search"> Search </a>
+            </li>
+        </ul>
+    </header>
+)
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {HashRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
@@ -7,10 +7,12 @@ import TV from "Routes/TV";
 
 export default () => (
     <Router>
-        <>
+        <Switch>
         <Route path="/" exact component = {Home}/>
         <Route path="/search" exact component = {Search}/>
-        <Route path="/tv" exact component = {TV}/>
-        </>
+        <Route path="/tv" component = {TV}/>
+        <Route path="/tv/popular" render = {() => <h1>Popular</h1>}/>
+        <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 );
