76d34c2d3af833d4527a5c282c33e9cbe8148984
kimmoya <kokokoro83@gmail.com>
2021-02-16 20:13:47 +0900

    mokoko

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 - [] TV Shows
 - [] Search
 - [] Detail
+
+
+## API Verbs
+
+- [X] Now Playing (Movie)
+- [X] Top Rated (Tv)
+- [X] Popular (Tv, Movie)
+- [X] Upcoming (Movie)
+- [X] Airing Today (TV)
+- [X] Tv Show detail
+- [X] movie Show detail
+- [X] Search ( Movies, TV)
---
+++ src/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:"https://api.themoviedb.org/3/",
+    params: {
+        api_key: "1afe4125f9fe1ed994f1e5514d597c47",
+        language: "en-US"
+    }
+});
+
+export const TVApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id => api.get(`tv/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search: term => api.get("search/movie", {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
+};
+
+export const MoviesApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: id => api.get(`movie/${id}` , {
+        params: {
+            append_to_response: "videos"
+        }
+    }) 
+};
