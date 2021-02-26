df6aa8ae34449746213d09e20dd3766f7994ea80
kimmoya <kokokoro83@gmail.com>
2021-02-26 09:23:17 +0900

    kokoko

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -11,12 +11,23 @@ export default class extends React.Component {
         error: null
     }
 
-    handleSubmit = () => {
+    handleSubmit = event => {
+        event.preventDefault();
         const { searchTerm } = this.state;
         if(searchTerm !== "") {
             this.searchByTerm();
         }
     };
+
+    updateTerm = event => {
+        const {
+          target: { value }
+        } = event;
+        this.setState({
+          searchTerm: value
+        });
+      };
+    
 
     searchByTerm = async() => {
         const { searchTerm } = this.state;
@@ -43,10 +54,11 @@ export default class extends React.Component {
         <SearchPresenter 
             movieResults = {movieResults}
             tvResults = {tvResults}
-            searchTerm = {searchTerm}
             loading = {loading}
             error = {error}
+            searchTerm = {searchTerm}
             handleSubmit = {this.handleSubmit}
+            updateTerm={this.updateTerm}
         />
         );
     }
